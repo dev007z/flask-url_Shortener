1c5dbb6e12bd1f30273405b91e2cfdd2e01681bd
Junaid Bashir <junaid@crayondevelopers.com>
2023-06-26 19:23:10 +0100

    import json create dictionary to store urls create json file to dump urls inside

--- url-shortener/url-short.py
+++ url-shortener/url-short.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__)
 
@@ -13,6 +14,14 @@ def index():
 def shortenUrl():
     # check form method and throw error if not POST
     if request.method == 'POST':
+        # create dictionary to store urls
+        urls = {}
+        urls[request.form["_code"]] = {'url': request.form['_url']}
+
+        # create json file
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
+
         return render_template('shortenUrl.html', code=request.form['_code'], url=request.form['_url'])
     else:
         # redirect to (the url for (index function))

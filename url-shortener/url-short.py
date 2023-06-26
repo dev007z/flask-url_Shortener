5a01a02069e21d95469a6adcdce228d264d49997
Junaid Bashir <junaid@crayondevelopers.com>
2023-06-26 18:45:26 +0100

    add comments import redirect, url_for modules redirect to index if request not POST

--- url-shortener/url-short.py
+++ url-shortener/url-short.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# route to main page
 @app.route('/')
 def index():
     return render_template('index.html',title="Home")
 
+# route to shortenUrl, on form submission
+#                           specify form methods
 @app.route('/shorten-url', methods=['GET', 'POST'])
 def shortenUrl():
+    # check form method and throw error if not POST
     if request.method == 'POST':
         return render_template('shortenUrl.html', code=request.form['_code'], url=request.form['_url'])
     else:
-        return "<h2>Invalid request. Change form method to POST</h2>"
+        # redirect to (the url for (index function))
+        return redirect(url_for('index'))

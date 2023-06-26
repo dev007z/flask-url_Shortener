cf152c49707ad5712b12580d27805573b9202ff1
Junaid Bashir <junaid@crayondevelopers.com>
2023-06-26 18:09:03 +0100

    add route to handle form import request module create shortenUrl page

--- url-shortener/url-short.py
+++ url-shortener/url-short.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html',title="Home")
+
+@app.route('/shorten-url')
+def shortenUrl():
+    return render_template('shortenUrl.html', code=request.args['_code'], url=request.args['_url'])

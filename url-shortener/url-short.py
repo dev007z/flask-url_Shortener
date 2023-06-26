b9df4d30acb1cfa0f05e3d678decb0edc6854610
Junaid Bashir <junaid@crayondevelopers.com>
2023-06-26 20:39:39 +0100

    add flash message to show record exists

--- url-shortener/url-short.py
+++ url-shortener/url-short.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import json, os.path as tap
 
 app = Flask(__name__)
+app.secret_key = 'abracadabra'
 
 # route to main page
 @app.route('/')
@@ -22,8 +23,9 @@ def shortenUrl():
             with open('urls.json', 'r') as urls_file:
                 urls = json.load(urls_file)
         if request.form['_code'] in urls.keys():
+            flash('Record already exists!')
             return redirect(url_for('index'))
-            
+
         urls[request.form["_code"]] = {'url': request.form['_url']}
         # create json file
         with open('urls.json', 'w') as url_file:
